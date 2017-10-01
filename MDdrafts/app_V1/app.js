8c7681d9f380ddf2d65d9ac3bc5252f01341949f
Cameron <cameron.grams.2@gmail.com>
2017-10-01 13:19:08 -0400

    build initial login static

---
+++ MDdrafts/app_V1/app.js
@@ -0,0 +1,45 @@
+const express = require( 'express' );
+const morgan = require( 'morgan' );
+
+const app = express();
+
+//routing info? 
+
+app.use( morgan( 'dev' ) );
+
+app.use( express.static( 'public' ) );
+
+app.get( '/', ( req, res ) => {
+  res.sendFile( _dirname + '/views/login.html' );
+} );
+
+
+
+let server;
+
+function runServer(){
+  const port = process.env.PORT || 8080;
+  return new Promise( ( resolve, reject ) => {
+    server = app.listen( port, () => {
+      console.log( `Listening on ${ port }` );
+      resolve( server );
+    } ).on( 'error' , err => {
+      reject( err )
+    } );
+  } );
+}
+
+function closerServer(){
+  return new Promise( ( resolve, reject ) => {
+    console.log( 'Closing Server' );
+    server.close( err => {
+      if ( err ){
+        reject( err );
+        return;
+      }
+      resolve();
+  } );
+}
+
+
+
