f14fbaf5036360ec32e2c527acc52ba9ea5a54c3
Cameron <cameron.grams.2@gmail.com>
2017-11-06 12:04:04 -0500

    fix the titles

--- userRouter.js
+++ userRouter.js
@@ -54,7 +54,7 @@ router.route('/user/authenticate')
                     var token = jwt.sign( 
                         { id: user._id, userName: user.name, level: user.level }, 
                         SECRET, {
-                        expiresIn: 60000000
+                        expiresIn: 600000000
                     } );
                     res.json( { success: true, token: 'Bearer ' + token, _id: user._id } );
                 } else {
